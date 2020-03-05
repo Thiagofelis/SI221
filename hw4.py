b418978caa0f51274d6be24a54dfb30a46f70397
Luciano Freitas <lucianofdz@gmail.com>
2020-03-05 19:55:36 +0100

    Generation Function as in the instructions

--- hw4.py
+++ hw4.py
@@ -5,13 +5,15 @@ import numpy as np
 def GeneratePoints(size, sigma):
     x1_1 = np.random.normal(1, sigma**2, int (size/2))
     x1_2 = np.random.normal(0, sigma**2, int (size/2))
-    x1 = np.concatenate ([x1_1, x1_2])
-    x2 = np.random.normal(0, sigma**2, size)
-    x = np.array((x1, x2)).T
+    x1 = np.array ((x1_1, x1_2)).T
+    x2_1 = np.random.normal(-1, sigma**2, int (size/2))
+    x2_2 = np.random.normal(0, sigma**2, int (size/2))
+    x2 = np.array ((x2_1, x2_2)).T
+    x = np.concatenate ([x1, x2])
     y_1 = [1 for i in x1_1]
     y_2 = [0 for i in x1_2]
     y = np.concatenate ([y_1, y_2])
     S = list(zip(x, y))
     return S
 
-print(GeneratePoints(10, 1))
+print(GeneratePoints(10, 0.01))
